a18667f7e5a8e796634ca0240c2714460efaa785
Thaís <parisethais@gmail.com>
2025-11-27 04:57:15 -0300

    fixed

--- car/serializers.py
+++ car/serializers.py
@@ -1,12 +1,18 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
+
 from .models import Car
 
 
 class CarSerializer(serializers.Serializer):
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(validators=[MaxValueValidator(1914), MinValueValidator(1)])
+    horse_powers = serializers.IntegerField(
+        validators=[
+            MaxValueValidator(1914),
+            MinValueValidator(1),
+        ]
+    )
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(null=True)
 
@@ -14,13 +20,25 @@ class CarSerializer(serializers.Serializer):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
-        instance.model = validated_data.get("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_power", instance.horse_power)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer,
+        )
+        instance.model = validated_data.get(
+            "model",
+            instance.model,
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers,
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken,
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description,
+        )
         instance.save()
         return instance
-
-
-
