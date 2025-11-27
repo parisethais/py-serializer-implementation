5b43712018068b5143f05c716bb786e7840377b6
Thaís <parisethais@gmail.com>
2025-11-27 04:51:31 -0300

    solution

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import json
 from car.models import Car
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    data = {
+        "manufacturer": car.manufacturer,
+        "model": car.model,
+        "horse_powers": car.horse_powers,
+        "is_broken": car.is_broken,
+        "problem_description": car.problem_description,
+    }
+    return json.dumps(data).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(car_bytes: bytes) -> Car:
+    json_str = car_bytes.decode("utf-8")
+    data = json.loads(json_str)
+
+    return Car(
+        manufacturer=data["manufacturer"],
+        model=data["model"],
+        horse_powers=data["horse_powers"],
+        is_broken=data["is_broken"],
+        problem_description=data["problem_description"],
+    )
